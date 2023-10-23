1b3cf9ad3ffc5756189fe268e2462e0047e8ef92
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-10-23 12:07:39 +0900

    a

--- main.cpp
+++ main.cpp
@@ -14,28 +14,6 @@ int GetRandom(int min, int max) {
 	return dist(gen);
 }
 
-//// コールバック関数の型を定義
-//typedef std::function<void(int, std::string)> CallbackFunction;
-//
-//// コールバック関数の実装
-//void ShowResult(int diceRoll, std::string userGuess) {
-//	std::cout << "サイコロの目: " << diceRoll << std::endl;
-//
-//	// 半か丁かを判定
-//	std::string result = (diceRoll % 2 == 0) ? "半" : "丁";
-//
-//	// 3秒待つ
-//	std::this_thread::sleep_for(std::chrono::seconds(3));
-//
-//	// 予想と結果を比較
-//	if (userGuess == result) {
-//		std::cout << "正解！" << std::endl;
-//	}
-//	else {
-//		std::cout << "不正解。" << std::endl;
-//	}
-//}
-
 int main() {
 	std::cout << "サイコロを振ります。半か丁か、どちらかを当ててください（半/丁）: ";
 	std::string userGuess;
@@ -44,7 +22,6 @@ int main() {
 	// サイコロを振る（1から6の範囲でランダムな数を生成）
 	int diceRoll = GetRandom(1, 6);
 
-	// コールバック関数をラムダ式で定義して呼び出す
 	std::function<void(int, std::string)> callback = [&userGuess](int diceRoll, std::string guess) {
 		std::cout << "サイコロの目: " << diceRoll << std::endl;
 

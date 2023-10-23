62449cb5886ed5768776e62033111b5feb092a4a
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-10-23 10:21:09 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,82 +1,18 @@
-//#include <stdio.h>
-//#include <random>
-//#include <windows.h>
-//
-//int GetRandom(int min, int max) {
-//	static int flag;
-//
-//	if (flag == 0) {
-//		srand((int)time(NULL));
-//		flag = 1;
-//	}
-//
-//	return min + (int)(rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
-//}
-//
-//typedef void (*PFunc)(char*);
-//
-//void setTimeout(PFunc p, int second) {
-//	Sleep(second * 1000);
-//
-//	//p(&second);
-//}
-//
-//void DispDinghan(char* s) {
-//	printf("%c！\n", *s);
-//}
-//
-//void Dinghan() {
-//	char dinghan{};
-//	scanf_s("%c", &dinghan);
-//
-//	int result = GetRandom(1, 6);
-//
-//	PFunc p;
-//
-//	if (result % 2 == 0) {
-//		p = DispDinghan;
-//		setTimeout(p, 3);
-//	}
-//	if (result % 2 == 1) {
-//		p = DispDinghan;
-//		setTimeout(p, 3);
-//	}
-//
-//	/*if (result % 2 == 0) {
-//		printf("丁!");
-//		if (dinghan == '丁') {
-//			printf("当たり！！");
-//		}
-//		if (dinghan == '半') {
-//			printf("はずれ...");
-//		}
-//	}
-//
-//	if (result % 2 == 1) {
-//		printf("半!");
-//		if (dinghan == '丁') {
-//			printf("はずれ...");
-//		}
-//		if (dinghan == '半') {
-//			printf("当たり！！");
-//		}
-//	}*/
-//}
-//
-//int main(void) {
-//	
-//	
-//
-//
-//
-//	return 0;
-//}
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <thread> // スリープ用
-#include <chrono> // スリープ用
+#include <thread>
+#include <chrono>
+#include <random>
+
+int GetRandom(int min, int max) {
+    static int flag;
+
+    if (flag == 0) {
+        srand((int)time(NULL));
+        flag = 1;
+    }
+}
 
 // コールバック関数の型を定義
 typedef void (*CallbackFunction)(int, std::string);
@@ -88,9 +24,10 @@ void ShowResult(int diceRoll, std::string userGuess) {
     // 半か丁かを判定
     std::string result = (diceRoll % 2 == 0) ? "半" : "丁";
 
-    std::this_thread::sleep_for(std::chrono::seconds(3)); // 3秒待つ
+    // 3秒待つ
+    std::this_thread::sleep_for(std::chrono::seconds(3)); 
 
-    // ユーザーの予想と結果を比較
+    // 予想と結果を比較
     if (userGuess == result) {
         std::cout << "正解！" << std::endl;
     }
@@ -100,15 +37,12 @@ void ShowResult(int diceRoll, std::string userGuess) {
 }
 
 int main() {
-    // 乱数初期化
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-
     std::cout << "サイコロを振ります。半か丁か、どちらかを当ててください（半/丁）: ";
     std::string userGuess;
     std::cin >> userGuess;
 
     // サイコロを振る（1から6の範囲でランダムな数を生成）
-    int diceRoll = std::rand() % 6 + 1;
+    int diceRoll = GetRandom(1, 6);
 
     // コールバック関数を呼び出す
     CallbackFunction callback = ShowResult;

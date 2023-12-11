d0b24276040cd51be5d3c588615857003e4e3b73
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2023-12-11 12:06:27 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <iostream>
+#include <list>
 
 int main(void) {
-	printf("好きな言葉");
+	std::list<const char*> stations{
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Nishi-Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaka",
+		"Shinagawa",
+		"Takanawa Geteway",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr) {
+		
+		std::cout << *itr << "\n";
+	}
+
 
 	return 0;
 }
